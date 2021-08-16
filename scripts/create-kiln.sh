d3a4ff87ca46c9c96efcdd5cd4f3adfdc7c7c1a5
Svetlozar Mihailov <svetliomihailov@gmail.com>
2021-08-16 21:10:03 +0300

    Add scripts for kiln repo creation

---
+++ scripts/create-kiln.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -X GET -v https://google.com
+# curl -X POST -v "http://localhost:9999/openrdf-workbench/repositories/NONE/create" \
+#   -H "Content-Type: application/x-www-form-urlencoded" \
+#   -d "type=memory-rdfs-dt&Repository+ID=kiln&Repository+title=&Persist=true&Sync+delay=0"
